d3ebcd6bfbd1a2cd1f9036cb99c45a1b2f72dcd1
snipeso <sophia.snipes@kispi.uzh.ch>
2020-03-05 17:55:56 +0100

    offiical light filtering script, including spot checking with plots

--- Preprocessing/B_LightFiltering.m
+++ Preprocessing/B_LightFiltering.m
@@ -1,15 +1,20 @@
 % resorts files by relevant folder, and gently filters things so they can
-% be marked for cutting and sleep scoring. 
+% be marked for cutting and sleep scoring.
 
 close all
 clc
 clear
 
 Refresh = false;
+SpotCheck = true;
 GeneralPreprocessingParameters
 
 for Indx_D = 1:size(Folders.Datasets,1) % loop through participants
     for Indx_F = 1:size(Folders.Subfolders, 1) % loop through all subfolders
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%
+        %%% Check if data exists
+        
         Path = fullfile(Paths.Datasets, Folders.Datasets{Indx_D}, Folders.Subfolders{Indx_F});
         
         % skip rest if folder not found
@@ -21,42 +26,83 @@ for Indx_D = 1:size(Folders.Datasets,1) % loop through participants
         Levels = split(Folders.Subfolders{Indx_F}, '\');
         Levels(cellfun('isempty',Levels)) = []; % remove blanks
         Levels(strcmpi(Levels, 'EEG')) = []; % remove uninformative level that its an EEG
- 
         
+        Task = Levels{1};
         
         % if does not contain EEG, then skip
         Content = ls(Path);
         SET = contains(string(Content), '.set');
         if ~any(SET)
-            if any(strcmpi(Levels, 'EEG'))
+            if any(strcmpi(Levels, 'EEG')) % if there should have been an EEG file, be warned
                 warning([Path, ' is missing SET file'])
             end
             continue
-        elseif nnz(SET) > 1
-            warning([Path, ' has more than one SET file']) %TODO, add to log
+        elseif nnz(SET) > 1 % if there's more than one set file, you'll need to fix that
+            warning([Path, ' has more than one SET file'])
             continue
         end
-         Filename.SET = Content(SET, :);
+        
+        Filename.SET = Content(SET, :);
+        
+        % set up destination location
+        Destination = fullfile(Paths.Preprocessed, Task);
+        Filename.Core = join([Folders.Datasets{Indx_D}, Levels(:)'], '_');
+        Filename.Destination = [Filename.Core{1}, '.set'];
+        
+        if ~exist(Destination, 'dir')
+            mkdir(Destination)
+        end
+        
+        % skip filtering if file already exists
+        if ~Refresh && exist(fullfile(Destination, Filename.Destination), 'file')
+            continue
+        end
+        
+        %%%%%%%%%%%%%%%%%%%
+        %%% filter the data
         
         EEG = pop_loadset('filepath', Path, 'filename', Filename.SET);
         
-            % notch filter
-%     fs = EEG.srate;
-%     wo = 50/(fs/2);
-%     bw = wo/15;
-%     [num,den] = iirnotch(wo,bw);
-%     EEG.data = filter(num,den,EEG.data);
-
-    % filter data
-    EEG = pop_eegfiltnew(EEG, low_cutoff, high_cutoff);
-    
-     % down-sample
-    EEG = pop_resample(EEG, new_fs);
-    EEG = eeg_checkset( EEG );
-%     
-    % filter data
-    EEG = pop_eegfiltnew(EEG, hp_eeg, lp_eeg);
-    
-    
+        SpotCheckOriginals = EEG.data(CheckChannels, :);
+        originalFS = EEG.srate;
+        
+        % notch filter for line noise
+        EEG = lineFilter(EEG, 'EU', false);
+        
+        % down-sample
+        EEG = pop_resample(EEG, new_fs);
+        EEG = eeg_checkset( EEG );
+        
+        % low-pass filter
+        EEG = pop_eegfiltnew(EEG, [], high_cutoff);
+        
+        % high-pass filter. NOTE: this is different from LP on purpose
+        EEG =  bandpassEEG(EEG, low_cutoff, []);
+        
+        % randomly check some of the datasets to make sure things look ok
+        if SpotCheck && randi(SpotCheckFrequency) == 1
+            SpotCheckFiltered = EEG.data(CheckChannels, :);
+            
+            % time vectors
+            tO = linspace(0, size(SpotCheckOriginals, 2)/originalFS, size(SpotCheckOriginals, 2));
+            tF = linspace(0, size(SpotCheckFiltered, 2)/EEG.srate, size(SpotCheckFiltered, 2));
+            
+            figure
+            for Indx_Ch = 1:numel(CheckChannels) % plot a subplot for each channel
+                subplot(numel(CheckChannels), 1, Indx_Ch)
+                hold on
+                plot(tO, SpotCheckOriginals(Indx_Ch, :), 'k')
+                plot(tF, SpotCheckFiltered(Indx_Ch, :), 'r')
+                title([Filename.Destination, ' ', num2str(CheckChannels(Indx_Ch))])
+                
+                A = 1; % wait with breakpoint, release BP when satisfied
+            end
+        end
+       
+        
+        pop_saveset(EEG, 'filename', Filename.Destination, ...
+                'filepath', Destination, ...
+                'check', 'on', ...
+                'savemode', 'onefile');
     end
 end
--- Preprocessing/GeneralPreprocessingParameters.m
+++ Preprocessing/GeneralPreprocessingParameters.m
@@ -1,13 +1,27 @@
+
+% locations
 Paths = struct();
 Folders = struct();
 
 Paths.Datasets = 'C:\Users\colas\Desktop\FakeData';
-Paths.Preprocessed = 'C:\Users\colas\Desktop\FakeDataPreprocessed';
+Paths.Preprocessed = 'C:\Users\colas\Desktop\FakeDataPreprocessedEEG';
 
 Folders.Template = 'PXX';
 Folders.Ignore = {'CSVs', 'other'};
 
-addpath(fullfile(cd, 'functions'))
+%%% parameters
 
+% light filtering
+new_fs = 500; % maybe 250?
+high_cutoff = 40;
+low_cutoff = 0.5;
+
+% spot checking
+SpotCheckFrequency = 1; % 1 out of this number will be plotted
+CheckChannels = [10, 70]; % frontal and occipital channel
+
+
+%%% Do stuff
+addpath(fullfile(cd, 'functions'))
 
 [Folders.Subfolders, Folders.Datasets] = AllFolderPaths(Paths.Datasets, Folders.Template, false, Folders.Ignore);
