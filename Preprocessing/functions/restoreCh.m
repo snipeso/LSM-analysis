3eb4c0fad28d771c3783f759a6ac10bcebb6f27d
snipeso <sophia.snipes@kispi.uzh.ch>
2020-03-06 18:00:13 +0100

    little fix

--- Preprocessing/functions/restoreCh.m
+++ Preprocessing/functions/restoreCh.m
@@ -4,7 +4,7 @@ m = matfile(CutFilename,'Writable',true);
 
 Content = whos(m);
 if ismember('badchans', {Content.name})
-   m.badchans(m.badchans == Ch) = [];
+   m.badchans(ismember(m.badchans, Ch)) = [];
 else
     m.badchans = Ch;
 end
