99ecb81d34b8a974dd529ee57d12fe3fcf9a2b25
snipeso <sophia.snipes@kispi.uzh.ch>
2020-03-07 20:14:05 +0100

    made script for auto cutting

---
+++ Preprocessing/functions/AutoCut.m
@@ -0,0 +1,134 @@
+function AutoCut(CutFilepath, Threshold, showPlots)
+
+% Procedure:
+% -
+
+% parameters
+NonEyeChannels = 27:128;
+Padding = 2; % in seconds, time window around edge of artefact to cut out
+minGap = 10; % in seconds, minimum time between artefacts to unify
+
+% load files
+m = matfile(CutFilepath,'Writable',true); % contains cutting info
+EEG = pop_loadset('filename', m.filename, 'filepath', m.filepath);
+
+fs = EEG.srate;
+[Channels, Points] = size(EEG.data);
+
+% average all channels together; this is so no one channel drives the cutting too much.
+meanChannel = mean(abs(EEG.data(NonEyeChannels, :)));
+
+% establish limits based on median and median absolute deviance; so extreme values don't push the limits too high
+medianVoltage = median(meanChannel);
+limit = medianVoltage*mad(meanChannel);
+
+% get all segments above the threshold
+AboveThresholds = meanChannel > limit;
+AboveThresholds = [0, AboveThresholds, 0]; % to make sure there's always a start and finish
+Segments = diff(AboveThresholds);
+Starts = find(Segments == 1);
+Ends = find(Segments == -1);
+
+
+% get all segments above the median
+MedianAboveThresholds = meanChannel > medianVoltage;
+MedianAboveThresholds = [0, MedianAboveThresholds, 0]; % to make sure there's always a start and finish
+MedianSegments = diff(MedianAboveThresholds);
+MedianStarts = find(MedianSegments == 1);
+MedianEnds = find(MedianSegments == -1);
+
+%%% expand every segment above threshold until it reaches closest value
+%%% that crosses median
+NewStarts = zeros(size(Starts));
+NewEnds = zeros(size(Ends));
+
+
+for Indx_I = 1:numel(Starts) % loop through all above-threshold segments
+    
+    % get new start
+    Start = Starts(Indx_I); % data goes above threshold
+    Previous = MedianStarts(MedianStarts<Start); % previous point in which data crossed median value
+    Start = Previous(end) - fs*Padding; % move start by padding value
+    
+    if Start <= 0 % handle edge case
+        Start = 1;
+    end
+    
+    NewStarts(Indx_I) = Start;
+    
+    % get new end
+    End = Ends(Indx_I);
+    Next = MedianEnds(MedianEnds>End);
+    End = Next(1) + fs*Padding;
+    
+    if End > Points
+        End = Points;
+    end
+    
+    NewEnds(Indx_I) = End;
+    
+    
+    
+end
+
+
+for Indx_I = 1:numel(NewStarts)
+    
+    NewStart = NewStarts(Indx_I);
+    NewEnd = NewEnds(Indx_I);
+    
+    %%% get all overlapping windows, and make one big window
+    OverlapWindowsIndx = ((NewStarts <= NewStart & NewEnds >= NewStart) | ...
+        (NewStarts <= NewEnd & NewEnds >= NewEnd));
+    
+    if ~any(OverlapWindowsIndx) % handle cases where there's no overlap
+        OverlapWindowsIndx = Indx_I;
+    end
+    
+    % set starts and ends of all overlapping windows to be the same,
+    % minimum value
+    OverlapStarts = NewStarts(OverlapWindowsIndx);
+    OverlapEnds = NewEnds(OverlapWindowsIndx);
+    
+    NewStart = min(OverlapStarts);
+    NewEnd = max(OverlapEnds);
+    
+    %%% get any neighboring windows, and merge if they're close
+    NearbyWindowsIndx = (NewEnds >= NewStart - minGap*fs & NewEnds <= NewEnd) | ...
+        (NewStarts <= NewEnd + minGap*fs & NewEnds >= NewStart); % get all windows within the neighbor zone
+    
+    NearbyStarts = NewStarts(NearbyWindowsIndx | OverlapWindowsIndx); % gather all the starts of either neighboring or overlapping windows
+    NearbyEnds = NewEnds(NearbyWindowsIndx | OverlapWindowsIndx);
+    
+    NewStart = min(NearbyStarts); % find earliest start
+    NewEnd = max(NearbyEnds);
+    
+    % set starts and ends to be the same
+    NewStarts(OverlapWindowsIndx | NearbyWindowsIndx) = NewStart;
+    NewEnds(OverlapWindowsIndx | NearbyWindowsIndx) = NewEnd;
+    
+end
+
+
+TotData = Points/fs;
+DataCutAuto =  sum(newWindows(:, 2) - newWindows(:, 1))/fs;
+
+
+
+if exist('showPlots', 'var')
+    
+    % plot all values of the average of the channels
+    figure
+    histogram(meanChannel)
+    
+    
+end
+
+% save
+Content = whos(m);
+if ismember('TMPREJ', {Content.name})
+    % add new windows to old ones
+    
+else
+    m.TMPREJ = NewTMPREJ;
+end
