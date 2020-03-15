5ff3864a27d63bee5d2c80eceadad509491d6931
snipeso <sophia.snipes@kispi.uzh.ch>
2020-03-15 12:06:00 +0100

    added untested logging

--- Preprocessing/A_BrainAmp2Set.m
+++ Preprocessing/A_BrainAmp2Set.m
@@ -11,13 +11,22 @@ GeneralPreprocessingParameters
 load('StandardChanlocs128.mat') % has channel locations in StandardChanlocs
 
 
+StartTIme = datestr(now, 'yy-mm-dd_HH-MM');
+m = matfile(fullfile(Paths.Datasets, StartTime, '_A_Log.mat'),'Writable',true);
+
+missing = struct();
+skipped = struct();
+converted = struct();
+
+A = tic;
 for Indx_D = 1:size(Folders.Datasets,1) % loop through participants
     for Indx_F = 1:size(Folders.Subfolders, 1) % loop through all subfolders
         Path = fullfile(Paths.Datasets, Folders.Datasets{Indx_D}, Folders.Subfolders{Indx_F});
         
         % skip rest if folder not found
         if ~exist(Path, 'dir')
-            % TODO: add to log
+            missing(end + 1).path = Path;
+            missing(end).reason = 'no path';
             warning([deblank(Path), ' does not exist'])
             continue
         end
@@ -32,11 +41,16 @@ for Indx_D = 1:size(Folders.Datasets,1) % loop through participants
         VHDR = contains(string(Content), '.vhdr');
         if ~any(VHDR)
             if any(strcmpi(Levels, 'EEG'))
-                warning([Path, ' is missing EEG files']) % TODO, add to log
+                missing(end+1).path = Path;
+                missing(end).reason = 'no raw eeg';
+                warning([Path, ' is missing EEG files'])
             end
             continue
         elseif nnz(VHDR) > 1
-            warning([Path, ' has more than one eeg file']) %TODO, add to log
+            skipped(end + 1).path = Path; %#ok<SAGROW>
+            skipped(end).files = Content(VHDR, :);
+            skipped(end).reason = 'more than one VHDR file';
+            warning([Path, ' has more than one eeg file'])
             continue
         end
         
@@ -48,31 +62,47 @@ for Indx_D = 1:size(Folders.Datasets,1) % loop through participants
         
         if not(Refresh) && any(strcmpi(Content, Filename.SET))
             disp(['***********', 'Already did ', Filename.Core, '***********'])
-            continue  % TODO, add to log that it was skipped
+            skipped(end + 1).path = Path; %#ok<SAGROW>
+            skipped(end).filename = Filename.SET;
+            skipped(end).reason = 'already done';
+            continue
         end
         
         try
             EEG = pop_loadbv(Path, Filename.VHDR);
         catch
             warning(['Failed to load ', Filename.VHDR]) % TODO, add to log
+            skipped(end + 1).path = Path; %#ok<SAGROW>
+            skipped(end).filename = Filename.VHDR;
+            skipped(end).reason = 'failed to load';
             continue
         end
         
         
         EEG.ref = 'CZ';
         EEG.chanlocs = StandardChanlocs;
+        EEG.info.oldname = filename.VHDR;
+        EEG.info.oldpath = Path;
         
         try
             pop_saveset(EEG, 'filename', Filename.SET, ...
                 'filepath', Path, ...
                 'check', 'on', ...
                 'savemode', 'onefile');
+            converted(end + 1).path = Path; %#ok<SAGROW>
+            converted(end).filename = Filename.SET;
         catch
             warning(['Failed to save ', Filename.Core]) % TODO, save to log
+            skipped(end + 1).path = Path; %#ok<SAGROW>
+            skipped(end).filename = Filename.VHDR;
+            skipped(end).reason = 'failed to save';
         end
-        % TODO: get from sven the thingy of loading dots to see progress
     end
-    
 end
+
+m.CleaningTime = toc(A);
+m.missing = missing;
+m.skipped = skipped;
+m.converted = converted;
 
 %TODO, create log with all parameters and outputs
