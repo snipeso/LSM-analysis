b77238d6e43e472a98ccd18c78caed9bd7161121
snipeso <snipeso@users.noreply.github.com>
2020-03-17 08:29:51 +0100

    adapted for brick

--- Preprocessing/GeneralPreprocessingParameters.m
+++ Preprocessing/GeneralPreprocessingParameters.m
@@ -3,29 +3,30 @@
 Paths = struct();
 Folders = struct();
 
-Paths.Datasets = 'C:\Users\colas\Desktop\FakeDataReal';
-Paths.Preprocessed = 'C:\Users\colas\Desktop\FakeDataRealPreprocessedEEG';
+Paths.Datasets = 'D:\LSM\data';
+Paths.Preprocessed = 'C:\Users\schlaf\Desktop\LSMpreprocessed';
 
 Folders.Template = 'PXX';
 Folders.Logs = 'PreprocessingLogs';
-Folders.Ignore = {'CSVs', Folders.Logs, 'other'};
+Folders.Ignore = {'CSVs', Folders.Logs, 'other', 'Lazy', 'P00'};
 
 %%% parameters
 
 % light filtering
-new_fs = 500; % maybe 250?
+new_fs = 256; % maybe 250?
 high_cutoff = 40;
 low_cutoff = 0.5;
 
 % spot checking
-SpotCheckFrequency = 1; % 1 out of this number will be plotted
+SpotCheckFrequency = 20; % 1 out of this number will be plotted
 CheckChannels = [10, 70]; % frontal and occipital channel
 
 
 %%% Do stuff
 addpath(fullfile(cd, 'functions'))
 
-[Folders.Subfolders, Folders.Datasets] = AllFolderPaths(Paths.Datasets, Folders.Template, false, Folders.Ignore);
+[Folders.Subfolders, Folders.Datasets] = AllFolderPaths(Paths.Datasets, ...
+    Folders.Template, false, Folders.Ignore);
 
 Paths.Logs = fullfile(Paths.Preprocessed, Folders.Logs);
 if ~exist(Paths.Logs, 'dir')
