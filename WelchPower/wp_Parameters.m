6b776f217a548bc17546df11dab2560af070a987
snipeso <sophia.snipes@kispi.uzh.ch>
2020-03-24 18:53:22 +0100

    listed channels to ignore

--- WelchPower/wp_Parameters.m
+++ WelchPower/wp_Parameters.m
@@ -7,6 +7,14 @@ Paths.EEGdata = fullfile(Paths.Preprocessing, 'Deblinked\', Task);
 
 Paths.powerdata = fullfile(Paths.Preprocessing, 'WelchPower', Task);
 
+% channels to ignore
+mastoids = [49, 56];
+EMG = [107, 113];
+face = [125, 126, 127, 128];
+ears  = [43, 48, 119, 120];
+neck = [63, 68, 73, 81, 88, 94, 99, 107, 113];
+notEEG = [mastoids, EMG, face, ears, neck];
+
 
 if ~exist(Paths.powerdata, 'dir')
     mkdir(Paths.powerdata) 
