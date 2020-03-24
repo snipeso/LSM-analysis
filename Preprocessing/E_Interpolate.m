3869e984d5c02f4b093ed6e2d74bb346af94af01
snipeso <sophia.snipes@kispi.uzh.ch>
2020-03-24 17:39:23 +0100

    commented script

--- Preprocessing/E_Interpolate.m
+++ Preprocessing/E_Interpolate.m
@@ -1,9 +1,11 @@
+% function that interpolates bad channels and bad little segments
+
 close all
 clc
 clear
 
-Target = 'LAT';
-Refresh = false;
+Target = 'LAT'; % specify folder for analysis
+Refresh = true;
 GeneralPreprocessingParameters
 
 % get files and paths
@@ -22,19 +24,18 @@ for Indx_F = 1:numel(Files) % loop through files in target folder
     
     %     % get filenames
     %     Filename_Source_EEG = Files{Indx_F};
-    Filename_Source_EEG = 'P03_LAT_Extras_ICAd.set';
+    Filename_Source_EEG = 'P03_LAT_Extras_ICAd.set'; %TEMP
     Filename_Cuts =  [extractBefore(Filename_Source_EEG,'_ICAd.set'), '_Cuts.mat'];
-    Filename_Destination = [extractBefore(Filename_Source_EEG,'.set'), '_ICAd.set'];
+    Filename_Destination = [extractBefore(Filename_Source_EEG,'.set'), '_Interped.set'];
     
-    %     % skip filtering if file already exists
-    %     if ~Refresh && exist(fullfile(Destination, Filename_Destination), 'file')
-    %         disp(['***********', 'Already did ', Filename_Destination, '***********'])
-    %         continue
-    %     elseif ~exist(fullfile(Source_Cuts, Filename_Cuts), 'file')
-    %         disp(['***********', 'No cuts for ', Filename_Destination, '***********'])
-    %         continue
-    %     end
-    
+    % skip filtering if file already exists
+    if ~Refresh && exist(fullfile(Destination, Filename_Destination), 'file')
+        disp(['***********', 'Already did ', Filename_Destination, '***********'])
+        continue
+    elseif ~exist(fullfile(Source_Cuts, Filename_Cuts), 'file')
+        disp(['***********', 'No cuts for ', Filename_Destination, '***********'])
+        continue
+    end
     
     % load cuts
     load(fullfile(Source_Cuts, Filename_Cuts))
@@ -42,30 +43,36 @@ for Indx_F = 1:numel(Files) % loop through files in target folder
         badchans = [];
     end
     
-    
     % load dataset
     EEG = pop_loadset('filepath', Source_EEG, 'filename', Filename_Source_EEG);
-    standardLocs = EEG.chanlocs;
     EEGnew = EEG;
-
     
-    % interpolate bad segments
-    % select a column of data, interpolate segment, as well as any bad
-    Segments = data2Segments(cutData);
-    Segments(ismember(Segments(:, 1), notEEG), :) = [];
-    %TODO: ignore all segments that are in channels that get ignored
-    Clusters = segments2clusters(Segments);
+    
+    %%% interpolate bad segments
+    
+    % get clusters of data to interpolate (overlapping segments)
+    Segments = data2Segments(cutData); % data is saved as nans with segments to cut out from lightly filtered
+    Segments(ismember(Segments(:, 1), notEEG), :) = []; % ignore segments that get cut out anyway because not EEG
+    Clusters = segments2clusters(Segments); % group segments into clusters based on temporal overlap
     
     for Indx_C = 1:size(Clusters, 2)
+        
+        % select the column of data of the current cluster
         Range = [Clusters(Indx_C).Start, Clusters(Indx_C).End];
         EEGmini =  pop_select(EEG, 'point', Range);
+        
+        % remove bad segment, and any bad channels and not eeg channels
         EEGmini = pop_select(EEGmini, 'nochannel', unique([Clusters(Indx_C).Channels, badchans, notEEG]));
+        
+        % interpolate bad segment
         EEGmini = pop_interp(EEGmini, EEG.chanlocs);
+        
+        % replace interpolated data into new data structure
         for Indx_Ch = 1:numel(Clusters(Indx_C).Channels)
             Ch = Clusters(Indx_C).Channels(Indx_Ch);
             EEGnew.data(Ch, Range(1):Range(2)) = EEGmini.data(Ch, :);
             
-            % temp
+            % TEMP
             figure
             hold on
             plot(EEG.data(Ch, Range(1):Range(2)))
@@ -74,11 +81,15 @@ for Indx_F = 1:numel(Files) % loop through files in target folder
     end
     
     % interpolate bad channels
-    EEGnew = pop_select(EEGnew, 'nochannel', badchans(~ismember(badchans, notEEG)));
-    EEGnew = pop_interp(EEGnew, standardLocs);
+    EEGnew = pop_select(EEGnew, 'nochannel', badchans(~ismember(badchans, notEEG))); % NOTE: this also takes out the not EEG channels and interpolates them; this is fine, we ignore it, but you have to remove them because otherwise they contribute to the interpolation
+    EEGnew = pop_interp(EEGnew, EEG.chanlocs);
     
     
-    
+    pop_saveset(EEGnew,  'filename', Filename_Destination, ...
+        'filepath', Destination, ...
+        'check', 'on', ...
+        'savemode', 'onefile', ...
+        'version', '7.3');
     
     % TODO: randomly plot, plot normal eeg with interpolated eeg on top
     eegplot(EEG.data, 'srate', EEG.srate, 'data2', EEGnew.data)
