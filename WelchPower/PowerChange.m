a4db8f04578b766375b8caaed224b6ad4633ad3c
snipeso <sophia.snipes@kispi.uzh.ch>
2020-03-27 18:49:50 +0100

    script for raw data

--- WelchPower/PowerChange.m
+++ WelchPower/PowerChange.m
@@ -14,31 +14,30 @@ ChanLabels = {Chanlocs.labels};
 Channels = size(Chanlocs, 2);
 Colors = MapRainbow([Chanlocs.X], [Chanlocs.Y], [Chanlocs.Z], true);
 
-normFFT = allFFT;
-for Indx_P = 1:numel(Participants)
-    for Indx_Ch = 1:Channels
-        BL_Indx = find(strcmp(Categories(1, :),Participants{Indx_P}) & strcmp(Categories(3, :), 'MainPre'));
-        BL = nanmean(allFFT(BL_Indx).FFT(Indx_Ch, :, :), 3);
-        
-        for Indx_S = 1:numel(Sessions)
-            F_Indx =  find(strcmp(Categories(1, :),Participants{Indx_P}) & strcmp(Categories(3, :), Sessions{Indx_S}));
-            if isempty(F_Indx)
-                if Indx_Ch == 1
-               disp(['**************Skipping ', Participants{Indx_P},  Sessions{Indx_S}, '*****************'])
-                end
-               continue
-            end
-            S = squeeze(allFFT(F_Indx).FFT(Indx_Ch, :, :));
-            normFFT(F_Indx).FFT(Indx_Ch, :, :) = 100*((S-BL')./BL');
-        end
-    end
-end
+% normFFT = allFFT;
+% for Indx_P = 1:numel(Participants)
+%     for Indx_Ch = 1:Channels
+%         BL_Indx = find(strcmp(Categories(1, :),Participants{Indx_P}) & strcmp(Categories(3, :), 'MainPre'));
+%         BL = nanmean(allFFT(BL_Indx).FFT(Indx_Ch, :, :), 3);
+%         
+%         for Indx_S = 1:numel(Sessions)
+%             F_Indx =  find(strcmp(Categories(1, :),Participants{Indx_P}) & strcmp(Categories(3, :), Sessions{Indx_S}));
+%             if isempty(F_Indx)
+%                 if Indx_Ch == 1
+%                disp(['**************Skipping ', Participants{Indx_P},  Sessions{Indx_S}, '*****************'])
+%                 end
+%                continue
+%             end
+%             S = squeeze(allFFT(F_Indx).FFT(Indx_Ch, :, :));
+%             normFFT(F_Indx).FFT(Indx_Ch, :, :) = 100*((S-BL')./BL');
+%         end
+%     end
+% end
 
 
 
 
 figure
-subplot(5, 9, 1:4*9)
 hold on
 allAverages = nan(Channels, numel(Sessions));
 Freq = 7;
@@ -49,12 +48,12 @@ for Indx_Ch = 1:Channels
         Session_Indexes = find(strcmp(Categories(3, :), Sessions{Indx_S}));
         
         for Indx_P = 2:numel(Session_Indexes)
-%             pAverages(Indx_P) = nanmean(allFFT(Session_Indexes(Indx_P)).FFT(Indx_Ch, FreqIndx, :), 3);
-            pAverages(Indx_P) = nanmean(normFFT(Session_Indexes(Indx_P)).FFT(Indx_Ch, FreqIndx, :), 3);
+            pAverages(Indx_P) = nanmean(allFFT(Session_Indexes(Indx_P)).FFT(Indx_Ch, FreqIndx, :), 3);
+%             pAverages(Indx_P) = nanmean(normFFT(Session_Indexes(Indx_P)).FFT(Indx_Ch, FreqIndx, :), 3);
         end
         
-%         allAverages(Indx_Ch, Indx_S) = log(nanmean(pAverages));
-        allAverages(Indx_Ch, Indx_S) = nanmean(pAverages);
+        allAverages(Indx_Ch, Indx_S) = log(nanmean(pAverages));
+%         allAverages(Indx_Ch, Indx_S) = nanmean(pAverages);
     end
     plot(allAverages(Indx_Ch,:), 'Color', Colors(Indx_Ch, :), 'LineWidth', 1)
 end
@@ -63,13 +62,16 @@ xticklabels({'BL', 'Pre', 'S1', 'S2-1', 'S2-2', 'S2-3', 'Post'})
 
 MeanAll = mean(mean(allAverages));
 StdAll = std(mean(allAverages));
-YLims = [MeanAll-3*StdAll,MeanAll+3*StdAll];
+% YLims = [MeanAll-3*StdAll,MeanAll+3*StdAll];
+YLims = [-1.5, .5];
 plot(repmat(1:7, 2, 1), repmat(YLims, 7, 1)', 'k', 'LineWidth', 2)
 ylim(YLims)
 xlim([0, 8])
 ylabel('Theta Power Density')
 
+figure
 for Indx_S = 1:size(allAverages, 2)
-    subplot(5, 9, 4*9+Indx_S+1)
+    subplot(1, size(allAverages, 2), Indx_S)
      topoplot(allAverages(:, Indx_S), Chanlocs, 'maplimits', YLims, 'style', 'map', 'headrad', 'rim')
+     title(Sessions{Indx_S})
 end
