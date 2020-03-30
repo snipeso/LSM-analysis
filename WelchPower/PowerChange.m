bf15c811d271c487ee108bddf7b3b56ff4c1653d
snipeso <sophia.snipes@kispi.uzh.ch>
2020-03-30 15:56:40 +0200

    plots frequencies of hotspot

--- WelchPower/PowerChange.m
+++ WelchPower/PowerChange.m
@@ -1,12 +1,12 @@
-% clear
-% clc
-% close all
-% 
-% wp_Parameters
-% 
-% 
-% [allFFT, Categories] = LoadAll(Paths.powerdata);
-% Chanlocs = allFFT(1).Chanlocs;
+clear
+clc
+close all
+
+wp_Parameters
+
+
+[allFFT, Categories] = LoadAll(Paths.powerdata);
+Chanlocs = allFFT(1).Chanlocs;
 
 Sessions = {'BaselineBeam', 'MainPre', 'Session1Beam', 'Session2Beam1', 'Session2Beam2', 'Session2Beam3', 'MainPost'};
 Participants = unique(Categories(1, :));
@@ -47,7 +47,7 @@ for Indx_Ch = 1:Channels
         pAverages = nan(numel(Participants), 1);
         Session_Indexes = find(strcmp(Categories(3, :), Sessions{Indx_S}));
         
-        for Indx_P = 1:numel(Session_Indexes)
+        for Indx_P = 2:numel(Session_Indexes)
             pAverages(Indx_P) = nanmean(allFFT(Session_Indexes(Indx_P)).FFT(Indx_Ch, FreqIndx, :), 3);
 %             pAverages(Indx_P) = nanmean(normFFT(Session_Indexes(Indx_P)).FFT(Indx_Ch, FreqIndx, :), 3);
         end
@@ -62,8 +62,8 @@ xticklabels({'BL', 'Pre', 'S1', 'S2-1', 'S2-2', 'S2-3', 'Post'})
 
 MeanAll = mean(mean(allAverages));
 StdAll = std(mean(allAverages));
-% YLims = [MeanAll-3*StdAll,MeanAll+3*StdAll];
-YLims = [-.8, 1.6];
+YLims = [MeanAll-3*StdAll,MeanAll+3*StdAll];
+% YLims = [-.8, 1.6];
 plot(repmat(1:7, 2, 1), repmat(YLims, 7, 1)', 'k', 'LineWidth', 2)
 ylim(YLims)
 xlim([0, 8])
@@ -77,38 +77,57 @@ for Indx_S = 1:size(allAverages, 2)
      title(Sessions{Indx_S})
 end
 
+figure
+ topoplot(allAverages(:, 6), Chanlocs, 'maplimits', YLims, 'electrodes', 'labels', 'style', 'map', 'headrad', 'rim')
+
 %% frequency
+
+
+Channels = [3:7, 9:13, 15, 16, 18:20, 24, 106,111, 112, 117, 118, 123, 124];
+
+ChanIndx = ismember( str2double({Chanlocs.labels}), Channels); % hotspot
+% ChanIndx = ~ismember( str2double({Chanlocs.labels}), Channels); % not hotspot
 
 
 figure
 hold on
-allAverages = nan(Channels, numel(Sessions));
-Freqs = 1:20;
-FreqIndx =  dsearchn( allFFT(1).Freqs', Freqs);
-for Indx_F = 1:Channels
+Freqs = [1:20];
+FreqIndx =  dsearchn( allFFT(1).Freqs', Freqs');
+allAverages = nan(numel(Freqs), numel(Sessions));
+%TODO: get colors
+Colors = colormap(flipud(jet(numel(Freqs))));
+for Indx_F = 1:numel(Freqs)
     for Indx_S = 1:numel(Sessions)
-        pAverages = nan(numel(Freqs), 1);
+        pAverages = nan(numel(Participants), numel(Channels));
         Session_Indexes = find(strcmp(Categories(3, :), Sessions{Indx_S}));
         
         for Indx_P = 1:numel(Session_Indexes)
-            pAverages(Indx_P) = nanmean(allFFT(Session_Indexes(Indx_P)).FFT(:, FreqIndx, :), 3);
-%             pAverages(Indx_P) = nanmean(normFFT(Session_Indexes(Indx_P)).FFT(Indx_Ch, FreqIndx, :), 3);
+             pAverages(Indx_P, :) = nanmean(allFFT(Session_Indexes(Indx_P)).FFT(ChanIndx, Indx_F, :), 3)';
+%             pAverages(Indx_P, :) = nanmean(normFFT(Session_Indexes(Indx_P)).FFT(ChanIndx, Indx_F, :), 3)';
         end
         
-        allAverages(Indx_Ch, Indx_S) = log(nanmean(pAverages));
-%         allAverages(Indx_Ch, Indx_S) = nanmean(pAverages);
+        allAverages(Indx_F, Indx_S) = log(nanmean(nanmean(pAverages, 1)));
+%         allAverages(Indx_F, Indx_S) = nanmean(nanmean(pAverages, 1));
     end
-    plot(allAverages(Indx_Ch,:), 'Color', Colors(Indx_Ch, :), 'LineWidth', 1)
+    plot(allAverages(Indx_F,:), 'o-', 'Color', Colors(Indx_F, :), 'LineWidth', 3)
 end
 xticks(1:7)
 xticklabels({'BL', 'Pre', 'S1', 'S2-1', 'S2-2', 'S2-3', 'Post'})
 
 MeanAll = mean(mean(allAverages));
 StdAll = std(mean(allAverages));
-% YLims = [MeanAll-3*StdAll,MeanAll+3*StdAll];
-YLims = [-.8, 1.6];
+YLims = [min(allAverages(:)), max(allAverages(:))];
 plot(repmat(1:7, 2, 1), repmat(YLims, 7, 1)', 'k', 'LineWidth', 2)
 ylim(YLims)
 xlim([0, 8])
-ylabel('Theta Power Density')
-% ylabel('% Change Theta from Pre')
+title('HotSpot Frequency Change')
+ylabel('(log) Power Density')
+c = colorbar;
+caxis([min(Freqs), max(Freqs)])
+
+c.Label.String = 'Frequencies (Hz)';
+% c.Label.FontSize = 10;
+% % ylabel('% Change from Pre')
+
+set(findall(gcf,'-property','FontSize'),'FontSize',12)
+
