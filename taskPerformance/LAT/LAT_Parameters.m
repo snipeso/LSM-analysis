ec7be25a39029fad42f7df05b1ce78cea51ffa8d
snipeso <sophia.snipes@kispi.uzh.ch>
2020-03-31 15:25:55 +0200

    started script for getting LAT scores

---
+++ taskPerformance/LAT/LAT_Parameters.m
@@ -0,0 +1,10 @@
+
+Paths = struct();
+Paths.Datasets = 'D:\LSM\data';
+Destination = '';
+
+
+
+
+addpath('C:\Users\colas\Projects\LSM-analysis\generalFunctions')
+
--- taskPerformance/LAT/importOutput.m
+++ taskPerformance/LAT/importOutput.m
@@ -1,4 +1,7 @@
-function Output = importOutput(filepath, OutputType)
+function Output = importOutput(filepath, OutputType, extraFields)
+% extraFields should be a 2 x n cell array, with the first row indicating
+% the field name, and the second the field value
+
 
 TrialStruct = struct();
 
@@ -31,8 +34,19 @@ for Indx_T = 1:numel(TextTrials)
         Struct.(Field{1}) = nan;
     end
     
+
+    
     % add current structure to ongoing trials structure
     TrialStruct(Indx_T)  = Struct;
+    
+        % Add extra fields to struct
+    if exist('extraFields', 'var') && numel(extraFields) > 0
+        for Indx_F = 1:size(extraFields, 2)
+
+           TrialStruct(Indx_T).(extraFields{1, Indx_F}) = extraFields{2, Indx_F};
+
+        end
+    end
 end
 
 % change output based on what was specified
