b0c730c2ea9bdd58b604c7f2a35d62c7b5c03a3a
snipeso <sophia.snipes@kispi.uzh.ch>
2020-04-02 13:44:00 +0200

    script for plotting kss

---
+++ Questionnaires/KSS/PlotKSS.m
@@ -0,0 +1,56 @@
+
+
+filename = 'LAT_All.csv';
+
+Answers = readtable(filename);
+
+KSS =  Answers.numAnswer(strcmp(Answers.qID, qID));
+
+Categories = {};
+
+
+% Sessions = {'BaselineBeam', 'Session1Beam', 'Session2Beam1',};
+% SessionLabels = {'BLb', 'S1b', 'S2b1',};
+
+Sessions = {'BaselineBeam', 'MainPre', 'Session1Beam', 'Session2Beam1', 'Session2Beam2', 'Session2Beam3', 'MainPost'};
+SessionLabels = {'BL', 'Pre', 'S1', 'S2-1', 'S2-2', 'S2-3', 'Post'};
+
+
+Participants = {'P01', 'P02', 'P03', 'P04', 'P05', 'P06', 'P07'};
+
+figure
+hold on
+KSSall = nan(numel(Participants), numel(Sessions));
+for Indx_P = 1:numel(Participants)
+   for Indx_S = 1:numel(Sessions)
+       KSS = Answers.numAnswer(strcmp(Answers.qID, qID) & ...
+           strcmp(Answers.dataset, Participants{Indx_P}) & ...
+           strcmp(Answers.Level2, Sessions{Indx_S}));
+       if numel(KSS) < 1
+           continue
+       end
+       KSS = 1 + KSS.*8; % convert to scale from 1 to 9;
+       
+       KSSall(Indx_P, Indx_S) = KSS;
+   end
+end
+
+figure
+hold on
+Color = [0.7, 0.7, 0.7];
+
+
+for Indx_P = 1:numel(Participants)
+    plot(KSSall(Indx_P, :), 'o-', 'LineWidth', 1, 'MarkerFaceColor', Color, 'Color', Color)
+end
+
+plot(nanmean(KSSall, 1), 'o-', 'LineWidth', 2, 'Color', 'k',  'MarkerFaceColor', 'k')
+title('KSS Scores')
+xlim([0, numel(Sessions) + 1])
+xticks(1:numel(Sessions))
+xticklabels(SessionLabels)
+ylim([0 10])
+yticks(1:9)
+
+
+
