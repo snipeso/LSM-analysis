87b0b9d829e1192a4244ed32727fbfb66ec6d35a
snipeso <sophia.snipes@kispi.uzh.ch>
2020-04-06 13:02:17 +0200

    now BAT also uses external functions for plots

--- Questionnaires/KSS/PlotBAT.m
+++ Questionnaires/KSS/PlotBAT.m
@@ -2,18 +2,28 @@ clear
 clc
 close all
 
+%TODO: something is wrong with the kss plot, maybe to do with "bat 6"
+
 Q_Parameters
 
-Figure_Path = fullfile(Figure_Path, 'BAT');
+Figure_Path = fullfile(Paths.Figures, 'BAT');
 
 filenames = {'LAT_All.csv', 'PVT_All.csv', 'Music_All.csv', 'Game_All.csv', ...
     'Match2Sample_All.csv', 'SpFT_All.csv'};
 
-Figures = struct();
-
+qIDs = {'BAT_1', 'BAT_3_0', 'BAT_3', 'BAT_3_1', 'BAT_4', 'BAT_4_1', 'BAT_5', 'BAT_8'};
+Titles = {'KSS';
+    'Frustrating-Relaxing';
+    'Boring-Interesting';
+    'Distracted-Focused';
+    'Easy-Difficult';
+    'No Effort-Effortful';
+    'Poor-Good Perform';
+    'No Motiv-Lots Motiv'};
 
 for Indx_T = 1:numel(filenames)
     
+    % use appropriate session names
     if contains(filenames{Indx_T}, 'LAT' )|| contains(filenames{Indx_T}, 'PVT' )
         Sessions = allSessions.Comp;
         SessionLabels = allSessionLabels.Comp;
@@ -23,75 +33,36 @@ for Indx_T = 1:numel(filenames)
     end
     
     
-    Answers = readtable(fullfile(CSV_Path, filenames{Indx_T}));
+    Answers = readtable(fullfile(Paths.CSV, filenames{Indx_T}));
     
     
     % Fix qID problem
-    
-    
-    %split Bat3
     Answers.qID(strcmp(Answers.qLabels, 'Frustrating/Neutral/Relaxing')) = {'BAT_3_0'};
-    
-    
-    qIDs = {'BAT_1', 'BAT_3_0', 'BAT_3', 'BAT_3_1', 'BAT_4', 'BAT_4_1', 'BAT_5', 'BAT_8'};
-    Titles = {'KSS';
-        'Frustrating-Relaxing';
-        'Boring-Interesting';
-        'Distracted-Focused';
-        'Easy-Difficult';
-        'No Effort-Effortful';
-        'Poor-Good Perform';
-        'No Motiv-Lots Motiv'};
-    
-    
-    
-    Participants = {'P01', 'P02', 'P03', 'P04', 'P05', 'P06', 'P07'};
-    
-    
-    Colors = [linspace(0, (numel(Participants) -1)/numel(Participants), numel(Participants))', ...
-        ones(numel(Participants), 1)*0.2, ...
-        ones(numel(Participants), 1)];
-    Colors = hsv2rgb(Colors);
-    
+
     Task = extractBefore(filenames{Indx_T}, '_');
     
     for Indx_Q = 1:numel(qIDs)
+
+        qID = qIDs{Indx_Q};
+         
+        % this was named differently just for P01
+        if strcmp(qID, 'BAT_1') && nnz(strcmp(Answers.qID, 'BAT_6'))
+            qID = 'BAT_6';
+        end
+        
+        [AnsAll, Labels] = TabulateAnswers(Answers, Sessions, Participants, qID, 'numAnswer');
+        AnsAll = 100*AnsAll;
+        
+        
         if Indx_T == 1
             figure('Name', Titles{Indx_Q}, 'units','normalized','outerposition',[0 0 .5 .7]);
         else
             figure(Indx_Q)
         end
+        
         subplot(2, 3, Indx_T)
-        hold on
-        AnsAll = nan(numel(Participants), numel(Sessions));
-        for Indx_P = 1:numel(Participants)
-            for Indx_S = 1:numel(Sessions)
-                
-                qID = qIDs{Indx_Q};
-                SessionIndexes = strcmp(Answers.dataset, Participants{Indx_P}) & strcmp(Answers.Level2, Sessions{Indx_S});
-                
-                if strcmp(qID, 'BAT_1') && nnz(strcmp(Answers.qID(SessionIndexes), 'BAT_6'))
-                    qID = 'BAT_6';
-                end
-                Ans = Answers.numAnswer(strcmp(Answers.qID, qID) & SessionIndexes);
-                if numel(Ans) < 1
-                    continue
-                end
-                AnsAll(Indx_P, Indx_S) = 100*Ans;
-            end
-        end
+        PlotConfettiSpaghetti(AnsAll, Sessions, SessionLabels, [0 100], Task, strsplit(num2str(0:20:100)))
         
-        
-        for Indx_P = 1:numel(Participants)
-            plot(AnsAll(Indx_P, :), 'o-', 'LineWidth', 1, 'MarkerFaceColor', Colors(Indx_P, :), 'Color', Colors(Indx_P, :))
-        end
-        
-        plot(nanmean(AnsAll, 1), 'o-', 'LineWidth', 2, 'Color', 'k',  'MarkerFaceColor', 'k')
-        xlim([0, numel(Sessions) + 1])
-        xticks(1:numel(Sessions))
-        xticklabels(SessionLabels)
-        ylim([0 100])
-        title([Task])
         if Indx_T == numel(filenames)
             FigureName =  [Titles{Indx_Q},  '_BAT.svg'];
             saveas(gcf,fullfile(Figure_Path, FigureName))
