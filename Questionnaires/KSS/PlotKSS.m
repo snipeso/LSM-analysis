79e76e7106e9cd81fded594c8aac8f85c64bcdbf
snipeso <sophia.snipes@kispi.uzh.ch>
2020-04-06 12:49:02 +0200

    used external plot functions

--- Questionnaires/KSS/PlotKSS.m
+++ Questionnaires/KSS/PlotKSS.m
@@ -1,52 +1,29 @@
 clear
 clc
 close all
+Q_Parameters
 
-filepath = 'C:\Users\colas\Projects\LSM-analysis\Questionnaires\CSVs';
-filename = 'PVT_All.csv';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Answers = readtable(fullfile(filepath, filename));
+filename = 'PVT_All.csv'; % choose which task to plot
+
+Figures_Path = fullfile(Paths.Figures, 'PVT'); % choose destination
+
+Sessions = allSessions.Comp; % choose which sessions to plot
+SessionLabels = allSessionLabels.Comp;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 qID = 'BAT_1';
 
-Sessions = allSessions.Comp;
-SessionLabels = allSessionLabels.Comp;
+% get KSS values
+Answers = readtable(fullfile(Paths.CSV, filename));
 
+[AnsAll, Labels] = TabulateAnswers(Answers, Sessions, Participants, qID, 'numAnswer');
 
+AnsAll = 1 + AnsAll.*8; % convert to scale from 1 to 9
 
-KSSall = nan(numel(Participants), numel(Sessions));
-for Indx_P = 1:numel(Participants)
-    for Indx_S = 1:numel(Sessions)
-        KSS = Answers.numAnswer(strcmp(Answers.qID, qID) & ...
-            strcmp(Answers.dataset, Participants{Indx_P}) & ...
-            strcmp(Answers.Level2, Sessions{Indx_S}));
-        if numel(KSS) < 1
-            continue
-        end
-        KSS = 1 + KSS.*8; % convert to scale from 1 to 9;
-        
-        KSSall(Indx_P, Indx_S) = KSS;
-    end
-end
-
-figure
-hold on
-Colors = [linspace(0, (numel(Participants) -1)/numel(Participants), numel(Participants))', ...
-    ones(numel(Participants), 1)*0.2, ...
-    ones(numel(Participants), 1)];
-Colors = hsv2rgb(Colors);
-
-for Indx_P = 1:numel(Participants)
-    plot(KSSall(Indx_P, :), 'o-', 'LineWidth', 1, 'MarkerFaceColor', Colors(Indx_P, :), 'Color', Colors(Indx_P, :))
-end
-
-plot(nanmean(KSSall, 1), 'o-', 'LineWidth', 2, 'Color', 'k',  'MarkerFaceColor', 'k')
-title('KSS Scores')
-xlim([0, numel(Sessions) + 1])
-xticks(1:numel(Sessions))
-xticklabels(SessionLabels)
-ylim([0 10])
+PlotConfettiSpaghetti(AnsAll, Sessions, SessionLabels, [0 10], Title, Labels)
 yticks(1:9)
 
-
-
+saveas(gcf,fullfile(Figures_Path, 'KSS_RRT.svg'))
