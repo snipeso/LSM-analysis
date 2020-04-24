fd4feb6bc596e5d59d844e06eff048003e5e36b7
snipeso <sophia.snipes@kispi.uzh.ch>
2020-04-24 11:13:29 +0200

    plot simulation of tiny change

--- WelchPower/LAT/fakedata.m
+++ WelchPower/LAT/fakedata.m
@@ -1,11 +1,14 @@
 
 clc
 close all
+% BL = [1000 500 250 125 60 30 15 7 3];
+% SD = [1000 500 250 125 60 60 30 8.4 3];
+% 
 BL = [1000 500 250 125 60 30 15 7 3];
-SD = [1000 500 250 125 60 60 30 20 3];
+SD = [2000 1000 300 125 60 30 15 7 3];
 Freqs = linspace(.5, 4, numel(SD));
-figure( 'units','normalized','outerposition',[0 0 .8 .5])
-subplot(1, 3, 1)
+figure( 'units','normalized','outerposition',[0 0 .5 .5])
+subplot(1, 2, 1)
 hold on
 plot(Freqs, BL, 'LineWidth', 2, 'Color', [0 1 0])
 plot(Freqs, SD, 'LineWidth', 2, 'Color', [1 0 1])
@@ -13,22 +16,12 @@ plot(Freqs, mean(BL)*ones(1, numel(SD)), ':', 'Color', [0 1 0], 'LineWidth', 2)
 plot(Freqs, mean(SD)*ones(1, numel(SD)), ':', 'Color', [1 0 1], 'LineWidth', 2)
 xlabel('Frequencies')
 ylabel('raw power')
-legend({'Fake Down Stim','Fake Up Stim'})
+legend({'Fake Sham','Fake Up Stim', 'delta sham', 'delta up stim'})
 title('Power')
 xlim([min(Freqs), max(Freqs)])
 
-subplot(1, 3, 2)
-hold on
-plot(Freqs, log(BL), 'LineWidth', 2, 'Color', [0 1 0])
-plot(Freqs, log(SD), 'LineWidth', 2, 'Color', [1 0 1])
-plot(Freqs, mean(log(BL))*ones(1, numel(SD)), ':', 'Color', [0 1 0], 'LineWidth', 2)
-plot(Freqs, mean(log(SD))*ones(1, numel(SD)), ':', 'Color', [1 0 1], 'LineWidth', 2)
-xlabel('Frequencies')
-ylabel('log power')
-legend({'Fake Down Stim','Fake Up Stim'})
-title('Log power')
-xlim([min(Freqs), max(Freqs)])
-subplot(1,3,3)
+
+subplot(1,2,2)
 hold on
 Change = 100*(SD-BL)./BL;
 plot(Freqs, Change, 'LineWidth', 2, 'Color', 'b')
@@ -39,13 +32,10 @@ mBL = mean(BL);
 ChangeMean = 100*(mSD-mBL)./mBL;
 plot(Freqs,  mean(ChangeMean)*ones(1, numel(SD)), 'Color', 'r', 'LineWidth', 2)
 
-mSD = mean(log(SD));
-mBL = mean(log(BL));
-ChangeMean = 100*(mSD-mBL)./mBL;
-plot(Freqs,  mean(ChangeMean)*ones(1, numel(SD)), 'Color', 'g', 'LineWidth', 2)
+
 xlim([min(Freqs), max(Freqs)])
 ylabel('% change')
-legend({'Change by freq', 'mean change by freq', 'mean change of delta', 'mean change of log of delta'})
+legend({'Change by freq', 'mean change by freq', 'mean change of delta'})
 title('Difference between Up and Down')
 
 
