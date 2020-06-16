16cd5dc07426e2109e8310519e564e91f9c5057b
snipeso <sophia.snipes@kispi.uzh.ch>
2020-06-16 13:44:10 +0200

    Create mi2_Scoring2Time.m

---
+++ eeg/EEG_preprocessing/mi2_Scoring2Time.m
@@ -0,0 +1,46 @@
+% script to convert microsleep file to time windows
+
+close all
+clc
+clear
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Targets = {'LAT', 'PVT'};
+Refresh = false;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+EEG_Parameters
+
+for Indx_T = 1:numel(Targets)
+    
+    Target = Targets{Indx_T};
+    
+    % get files and paths
+    Source = fullfile(Paths.Preprocessed, 'Microsleeps', 'MAT', Target, 'Jelena', 'MSE');
+    Destination = fullfile(Paths.Preprocessed, 'Microsleeps', 'Scoring', Target);
+    
+    if ~exist(Destination, 'dir')
+        mkdir(Destination)
+    end
+    
+    Files = deblank(cellstr(ls(Source)));
+    Files(~contains(Files, '.mat')) = []; % remove extra files and . .. 
+    
+    for Indx_F = 1:numel(Files) % loop through files in target folder
+        
+        % get filenames
+        Filename = Files{Indx_F};
+        
+        % skip filtering if file already exists
+        if ~Refresh && exist(fullfile(Destination, Filename), 'file')
+            disp(['***********', 'Already did ', Filename, '***********'])
+            continue
+        end
+        
+        load(fullfile(Source, Filename), 'MSE_scoring')
+        
+        TimeWindows = data2windows(MSE_scoring);
+        
+        
+    end
+end
