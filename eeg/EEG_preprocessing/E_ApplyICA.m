73461e7e2d003bf128c69c868d162f3335c997a3
snipeso <snipeso@users.noreply.github.com>
2020-06-24 19:42:11 +0200

    automatically remove components that were selected

--- eeg/EEG_preprocessing/E_ApplyICA.m
+++ eeg/EEG_preprocessing/E_ApplyICA.m
@@ -3,9 +3,9 @@ clc
 clear
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Targets = 'LAT';
+Targets = {'LAT'};
 Data = 'Wake';
-Refresh = false;
+Refresh = true;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 EEG_Parameters
@@ -14,42 +14,56 @@ for Indx_T = 1:numel(Targets)
     Target = Targets{Indx_T};
     
     % get files and paths
-    Source_Components = fullfile(Paths.Preprocessed, 'ICA', 'Components', Target);
+    Source_ComponentData = fullfile(Paths.Preprocessed, 'ICA', 'Components', Target);
+    Source_Components = fullfile(Paths.Preprocessed, 'ICA', 'BadComponents', Target);
     Source_Data = fullfile(Paths.Preprocessed, Data, 'SET', Target);
-    Destination = fullfile(Paths.Preprocessed, 'ICA', 'ToDeblink', Target);
+    Destination = fullfile(Paths.Preprocessed, 'ICA', 'Deblinked', Target);
     
     if ~exist(Destination, 'dir')
         mkdir(Destination)
     end
     
-    Files = deblank(cellstr(ls(Source_Components)));
+    Files = deblank(cellstr(ls(Source_ComponentData)));
     Files(~contains(Files, '.set')) = [];
     
     for Indx_F = 1:numel(Files) % loop through files in target folder
         
         % get filenames
-        Filename_Components = Files{Indx_F};
-        Filename_Data = replace(Filename_Components, 'ICA_Components', Data);
-        Filename_Destination = [extractBefore(Filename_Data, Data), '.set'];
+        Filename_ComponentData = Files{Indx_F};
+        Filename_Data = replace(Filename_ComponentData, 'ICA_Components', Data);
+        Filename_Components = replace(Filename_Data, '.set', '.mat');
+        Filename_Destination = [extractBefore(Filename_Data, Data), 'Deblinked.set'];
         
         % skip if file already exists
         if ~Refresh && exist(fullfile(Destination, Filename_Destination), 'file')
             disp(['***********', 'Already did ', Filename_Destination, '***********'])
             continue
         elseif ~exist(fullfile(Source_Data, Filename_Data), 'file')
-            disp(['***********', 'No cuts for ', Filename_Destination, '***********'])
+            disp(['***********', 'No data for ', Filename_Destination, '***********'])
+            continue
+        elseif ~exist(fullfile(Source_Components, Filename_Components), 'file')
+            disp(['***********', 'No badcomps for ', Filename_Destination, '***********'])
             continue
         end
         
         % load dataset
-        EEG = pop_loadset('filepath', Source_Components, 'filename', Filename_Source);
+        EEG = pop_loadset('filepath', Source_ComponentData, 'filename', Filename_ComponentData);
         Data = pop_loadset('filepath', Source_Data, 'filename', Filename_Data);
+        load(fullfile(Source_Components, Filename_Components), 'badcomps')
+        
+        % remove channels from Data that aren't in EEG
+        
+        Data = pop_select(Data, 'channel', labels2indexes({EEG.chanlocs.labels}, Data.chanlocs));
         
         % rereference to average
         Data = pop_reref(Data, []);
         
         % merge sets
         EEG.data = Data.data;
+        
+        % remove bad components
+        EEG = pop_subcomp(EEG, badcomps);
+        
         
         % save new dataset
         pop_saveset(EEG, 'filename', Filename_Destination, ...
---
+++ functions/eeg/labels2indexes.m
@@ -0,0 +1,17 @@
+function Indexes = labels2indexes(Labels, Chanlocs)
+% function for converting from labels to indexes
+
+
+Labels = string(Labels);
+
+ChanLabels = string({Chanlocs.labels});
+
+[Members, Indexes] = ismember(Labels, ChanLabels);
+
+Indexes(Indexes == 0) = [];
+
+if any(not(Members))
+    warning(strjoin([ 'Chan ', Labels(not(Members)), ' not present in the chanlocs']))
+end
+
+
