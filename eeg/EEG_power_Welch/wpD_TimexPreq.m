68841624c294738df3f19cfd5488ae65a60772d6
snipeso <snipeso@users.noreply.github.com>
2020-06-26 17:38:58 +0200

    Create wpD_TimexPreq.m

---
+++ eeg/EEG_power_Welch/wpD_TimexPreq.m
@@ -0,0 +1,62 @@
+CLimsInd = [min(Quantiles(:, :, 1),[],  2), max(Quantiles(:, :, 2),[],  2)];
+
+
+plotChannels = [3:7, 9:13, 15, 16, 18:20, 24, 106, 111, 112, 117, 118, 123, 124]; % hotspot
+
+
+% plot time x freq of recordings TODO: move?
+
+
+YLimFreq = [4 14];
+ChanIndx = ismember( str2double({Chanlocs.labels}), plotChannels);
+NotChanIndx =  ~ismember( str2double({Chanlocs.labels}), plotChannels); % not hotspot
+Title = 'HotSpot';
+
+for Indx_H = 1:2
+    if Indx_H == 1
+        finalChanIndx = ChanIndx;
+        Title =  'HotSpot';
+    else
+        finalChanIndx = NotChanIndx;
+        Title =  'Not HotSpot';
+    end
+    
+    figure( 'units','normalized','outerposition',[0 0 1 1])
+    
+    for Indx_P = 1:numel(Participants)
+        for Indx_S = 1:numel(Sessions)
+            if isempty(PowerStruct(Indx_P).(Sessions{Indx_S}))
+                continue
+            end
+            A = PowerStruct(Indx_P).(Sessions{Indx_S});
+            subplot(numel(Participants), numel(Sessions), numel(Sessions) * (Indx_P - 1) + Indx_S )
+            
+            PlotSessionFreqs(squeeze(nanmean(A(finalChanIndx, :, :), 1)), YLimFreq, CLimsInd(Indx_P, :), Freqs )
+            title([Participants{Indx_P}, ' ', Title, ' ', Sessions{Indx_S}])
+        end
+        
+    end
+    saveas(gcf,fullfile(Paths.Figures, [TitleTag,'_', Title, '_LAT_TimeFreq.svg']))
+end
+
+
+YLimFreq = [2 20];
+
+for Indx_P = 1:numel(Participants)
+    figure( 'units','normalized','outerposition',[0 0 .5 .5])
+    
+    for Indx_S = 1:numel(Sessions)
+        subplot(2,numel(Sessions), Indx_S)
+        A = PowerStruct(Indx_P).(Sessions{Indx_S});
+        PlotSessionFreqs(squeeze(nanmean(A(ChanIndx, :, :), 1)), YLimFreq, CLimsInd(Indx_P, :), Freqs )
+        title([Participants{Indx_P}, ' Hotspot ', SessionLabels{Indx_S}])
+        colorbar
+        subplot(2,numel(Sessions), numel(Sessions)+ Indx_S)
+        A = PowerStruct(Indx_P).(Sessions{Indx_S});
+        PlotSessionFreqs(squeeze(nanmean(A(NotChanIndx, :, :), 1)), YLimFreq, CLimsInd(Indx_P, :), Freqs )
+        title([Participants{Indx_P}, ' NotHotspot ', SessionLabels{Indx_S}])
+        colorbar
+    end
+    
+    saveas(gcf,fullfile(Paths.Figures, [TitleTag,'_', Participants{Indx_P}, '_LAT_TimeFreq.svg']))
+end
