f9b7a85cb2c69742dbc1d5b2e299e0b9c4bbcc7c
snipeso <sophia.snipes@kispi.uzh.ch>
2020-08-07 15:29:46 +0200

    serpately save data and power

--- eeg/EEG_ERPs/Load_Tones.m
+++ eeg/EEG_ERPs/Load_Tones.m
@@ -9,8 +9,13 @@ ERP_Parameters
 
 Task = 'LAT';
 
-Condition = 'AllBeam';
-Title = 'AllBeam';
+% Condition = 'AllBeam';
+% Title = 'AllBeam';
+
+
+Condition = 'Comp';
+Title = 'Classic';
+
 Refresh = false;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -24,39 +29,48 @@ if ~exist(Paths.Figures, 'dir')
 end
 
 load('Chanlocs111.mat', 'Chanlocs')
-    
+
 % get data by loading all files, or by opening existing file
-Struct_Path = fullfile(Paths.Summary, [Title, '_', Task, '_FFT.mat']);
-if ~exist(Struct_Path, 'file') || Refresh
+Struct_Path_Data = fullfile(Paths.Summary, [Title, '_', Task, '_Data.mat']);
+if ~exist(Struct_Path_Data, 'file') || Refresh
     disp('*************Creating allTones********************')
-
-
+    
+    
     Path = fullfile(Paths.ERPs, 'Tones', Task);
     Files = deblank(cellstr(ls(Path)));
     Files(~contains(Files, '.mat')) = [];
     
-    allTones = struct();
-
+    
+    allData =struct();
+    allPhase =struct();
+    allPower =struct();
+    
     for Indx_P = 1:numel(Participants)
         for Indx_S = 1:numel(Sessions)
-             File = Files(contains(Files, Sessions{Indx_S}) & contains(Files, Participants{Indx_P}));
-             if isempty(File)
-               warning(['**************Could not find ', Participants{Indx_P}, ' ',  Sessions{Indx_S}, '*************' ])
-                 continue
-             end
-             m = matfile(fullfile(Path, File{1}),  'Writable', false );
-             allTones(Indx_P).(Sessions{Indx_S}).Data = m.Data;
-              allTones(Indx_P).(Sessions{Indx_S}).Phase = m.Phase;
-                allTones(Indx_P).(Sessions{Indx_S}).Power = m.Power; 
-                
+            File = Files(contains(Files, Sessions{Indx_S}) & contains(Files, Participants{Indx_P}));
+            if isempty(File)
+                warning(['**************Could not find ', Participants{Indx_P}, ' ',  Sessions{Indx_S}, '*************' ])
+                continue
+            end
+            m = matfile(fullfile(Path, File{1}),  'Writable', false );
+            allData(Indx_P).(Sessions{Indx_S}) = m.Data;
+            allPhase(Indx_P).(Sessions{Indx_S})= m.Phase;
+            allPower(Indx_P).(Sessions{Indx_S}) = m.Power;
+            
         end
     end
-
     
-    save(Struct_Path, 'allTones', '-v7.3')
+    
+    save(Struct_Path_Data, 'allData', '-v7.3')
+    save(fullfile(Paths.Summary, [Title, '_', Task, '_Phase.mat']), 'allPhase')
+    save(fullfile(Paths.Summary, [Title, '_', Task, '_Power.mat']), 'allPower', '-v7.3')
 else
     disp('***************Loading allTones*********************')
     load(Struct_Path, 'allTones')
+    
+    load(Struct_Path_Data, 'allData')
+    load(fullfile(Paths.Summary, [Title, '_', Task, '_Phase.mat']), 'allPhase')
+    load(fullfile(Paths.Summary, [Title, '_', Task, '_Power.mat']), 'allPower')
 end
 
 
