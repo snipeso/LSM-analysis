b99a0d0ed61cc733cca99503e1bdc2a8c01a65d8
snipeso <sophia.snipes@kispi.uzh.ch>
2020-09-30 10:28:08 +0200

    fixed dendro plot

--- eeg/EEG_ICA_clustering/PlotDendro.m
+++ eeg/EEG_ICA_clustering/PlotDendro.m
@@ -1,20 +1,25 @@
 function PlotDendro(Links, Labels)
 figure('units','normalized','outerposition',[0 0 1 1])
-Dendro = dendrogram(Links, 0,'Labels', Labels);
+[Dendro, ~, Order] = dendrogram(Links, 0,'Labels', Labels);
 
-nLeaves =  size(Links)+1;
+nLeaves =  size(Links, 1)+1;
+hold on
 for Indx = 1:numel(Dendro)
    X = mean(Dendro(Indx).XData);
    
    Y = max(Dendro(Indx).YData);
-   scatter(X, Y, 30, 'EdgeColor', 'k', 'FaceColor', 'w', 'LineWidth', 2)
+%    scatter(X, Y, 30, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'w', 'LineWidth', 2)
    text(X,Y, num2str(Indx + nLeaves))
     
 end
 
-
+% write number of different component
+for Indx = 1:nLeaves
+    text(Indx, 0, num2str(Order(Indx)))
+    
+end
 
 YMax = max(Links(:, 3));
 
-Padding = YMax*.5;
+Padding = YMax*.05;
 ylim([0-Padding, YMax + Padding])
